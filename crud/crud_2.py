b609b7f63f4da9433994827a2eedd19d3754e507
Jamily Silva <dev.jamilyemanuele@gmail.com>
2026-02-05 17:43:53 -0300

    finalização de crud refinado
    refinamos, só dava para quebrar mais em relação a colocar string em local que é só int, mas isso fazemos no proximo

--- crud/crud_2.py
+++ crud/crud_2.py
@@ -1,78 +1,115 @@
 #CRUD de Produtos 
-def cadastrar_produto():
+def cadastrar_produto(produtos):
     #{"id": 1, "nome": "", "preco": 0.0, "estoque": 0}
     print('--- Cadastro Produtos ---')
-    id = len(produtos) + 1 
+    if len(produtos) == 0:
+        id = 1
+    else:
+        maior_id = max(p['id'] for p in produtos)
+        id = maior_id + 1
     print(f'ID Produto : {id}')
-    nome = input('Nome do Produto : ').capitalize()
-    preco = float(input('Preço : R$'))
-    estoque = int(input('Estoque : '))
-    produto = {"ID": id, "Nome": nome, "Preço": preco, "Estoque": estoque}
-    produtos.append(produto)
+    nome = input('Nome do Produto : ').lower()
+    preco = valida_float('Preço : ')
+    estoque = valida_int('Estoque : ')
+    produto = {"id": id, "nome": nome, "preço": preco, "estoque": estoque}
+    if buscar_produto(nome, produtos):
+        print('Dado duplicado!')
+        print('Altere ou Remova!')
+        return
+    else:
+        produtos.append(produto)
 
 
-def listar_produtos(produto):
+def listar_produtos(produtos):
     print('--- Produtos ---')
-    if len(produto) >= 1:
+    if len(produtos) > 0:
         for produto in produtos:
-            print(f'ID : {produto['ID']} | Nome : {produto['Nome']} | Preço : {produto['Preço']} | Estoque : {produto['Estoque']}')
+            print(f'Id : {produto['id']} | Nome : {produto['nome']} | Preço : {produto['preço']} | Estoque : {produto['estoque']}')
     else:
         print('Lista de produtos vazia')
 
 
-def atualizar_produto(produtos):
+def atualizar_produto():
     print('--- Editar Produtos ---')
-    produto = input('Informe o nome do produto para atualizar: ').capitalize()
-    for p in produtos:
-        if p['Nome'] == produto:
-            print('---|Produto|---')
-            print(f'1 - ID : {p['ID']}')
-            print(f'2 - Nome : {p['Nome']}')
-            print(f'3 - Preço : : {p['Preço']}')
-            print(f'4 - Estoque : {p['Estoque']}')
-            dado = int(input('Informe o numero do dado a ser atualizado: '))
-            while True:
-                if dado == 1:
-                    dado_att = int(input('Novo ID: '))
-                    p['ID'] = dado_att
-                    print(f'ID atualizado com sucesso!')
-                    print(p)
-                    return
-                elif dado == 2:
-                    dado_att = input('Novo Nome: ').capitalize()
-                    p['Nome'] = dado_att
-                    print(f'Nome atualizado com sucesso!')
-                    print(p)
-                    return
-                elif dado == 3:
-                    dado_att = float(input('Novo Preço: '))
-                    p['Preço'] = dado_att
-                    print(f'Preço atualizado com sucesso!')
-                    print(p)
-                    return
-                elif dado == 4:
-                    dado_att = int(input('Quantia em Estoque: '))
-                    p['Estoque'] = dado_att
-                    print(f'Estoque atualizado com sucesso!')
-                    print(p)
-                    return
-                else:
-                    print('Opção inválida! Retornando')
-                    break
+    produto = input('Informe o nome do produto para atualizar: ').lower()
+    p = buscar_produto(produto, produtos)
+    if p:
+        print('---|Produto|---')
+        print(f'1 - ID : {p['id']}')
+        print(f'2 - Nome : {p['nome']}')
+        print(f'3 - Preço : : {p['preço']}')
+        print(f'4 - Estoque : {p['estoque']}')
+        dado = int(input('Informe o numero do dado a ser atualizado: '))
+        if dado == 1:
+            dado_att = int(input('Novo ID: '))
+            p['id'] = dado_att
+            print(f'ID atualizado com sucesso!')
+            print(p)
+            return
+        elif dado == 2:
+            dado_att = input('Novo Nome: ').lower()
+            p['nome'] = dado_att
+            print(f'Nome atualizado com sucesso!')
+            print(p)
+            return
+        elif dado == 3:
+            dado_att = valida_float('Novo Preço: ')
+            p['preço'] = dado_att
+            print(f'Preço atualizado com sucesso!')
+            print(p)
+            return
+        elif dado == 4:
+            dado_att = valida_int('Quantia em Estoque: ') 
+            p['estoque'] = dado_att
+            print(f'Estoque atualizado com sucesso!')
+            print(p)
+            return
+        else:
+            print('Opção inválida! Retornando')
+            return
     else :
         print('Produto não encontrado. Retornando a página principal')      
 
-def deletar_produto(produtos):
+def deletar_produto():
     print('--- Excluir Produto ---')
-    produto = input('Informe o nome do produto para a exclusão: ').capitalize()
-    for p in produtos:
-        if p['Nome'] == produto:
-           print(f'Deletando {p}')
-           produtos.remove(p)
-           return
+    produto = input('Informe o nome do produto para a exclusão: ').lower()
+    p = buscar_produto(produto, produtos)
+    if p:
+        print(f'Deletando {p}')
+        produtos.remove(p)
+        return
     else:
         print('Produto não encontrado. Por favor, verifique a informação e tente novamente!')
-            
+
+
+# funçoes auxiliadoras 
+
+def buscar_produto(produto, produtos): #Parâmetro é aquilo que MUDA de uma execução para outra. Dados mudam, funçoes não
+    for p in produtos:
+        if p['nome'] == produto:  
+            return p
+    return 
+
+
+def valida_int(mensagem):
+    while True:
+        estoque = input(mensagem)
+        try:
+            estoque = int(estoque)
+            return estoque
+        except ValueError:
+            print("Valor inválido. Digite um número inteiro.")
+
+
+def valida_float(mensagem):
+    while True:
+        preco = input(mensagem)
+        try:
+            preco = float(preco)
+            return preco
+        except ValueError:
+            print("Valor inválido. Digite um número natural.")
+
 
 produtos = []
 while True:
@@ -85,18 +122,23 @@ while True:
 
     opcao = int(input('Opção: '))
     if opcao == 1:
-        cadastrar_produto()
+        cadastrar_produto(produtos)
     elif opcao == 2:
         listar_produtos(produtos)
     elif opcao == 3:
-        atualizar_produto(produtos)
+        atualizar_produto()
     elif opcao == 4:
-        deletar_produto(produtos)
+        deletar_produto()
     elif opcao == 0 :
         print('Saindo do Sistema, Até mais!!')
         break
-    elif opcao != 1 or opcao != 2 or opcao != 3 or opcao != 4 or opcao != 0:
+    else: # ou elif opcap not in(0,1,2,3,4):
         print('Opção Inválida!')
         print('Saindo do Sistema, Até mais!!')
         break
 
+
+# validação de erro em inputs com int e float
+# Id baseado no maior Id da lista
+# lista padronizada lower()
+# sem produtos duplicados

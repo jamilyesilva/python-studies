45afb80ccd4a647950aa1ad5b019a0115e7f365e
Jamily Silva <dev.jamilyemanuele@gmail.com>
2026-02-11 06:05:04 -0300

    terminando essa part

--- crud/crud_3.py
+++ crud/crud_3.py
@@ -41,7 +41,7 @@ def status(mensagem):
         
 
 # funçoes create and read
-def nova_tarefa(): # dps adicionar um mensagem aqui e trocar o print, pra fazer função de update tarefa e nova tarefa
+def nova_tarefa(): 
     tarefas = abrir_arquivo()
     nova_tarefa = {}
     print('--- Nova Tarefa ---')
@@ -56,22 +56,45 @@ def nova_tarefa(): # dps adicionar um mensagem aqui e trocar o print, pra fazer 
         nova_tarefa["concluida"] = status('Tarefa concluída? (s/n): ')
         tarefas.append(nova_tarefa)
         with open("crud/tarefa.json", "w", encoding="utf-8") as arqv:
-            json.dump(tarefas, arqv, ensure_ascii=False, indent=4)
+            json.dump(tarefas, arqv, ensure_ascii=False, indent=4) #dump = objeto para json
         print('Tarefa criada com sucesso!')
     else:
         print('O título não pode ser vazio! Por favor, tente novamente.')
         return
     
-        
-        
-        
-    
+
+def concluir_tarefa(tarefas):
+    print('--- Editar Tarefa ---')
+    opcao = valida_int('Infome o id da tarefa para concluir: ')
+    for tarefa in tarefas:   
+        if opcao == tarefa["id"]:
+            tarefa["concluida"] = True
+            with open("crud/tarefa.json", "w", encoding="utf-8") as arqv:
+                json.dump(tarefas, arqv, ensure_ascii=False, indent=4)
+            print('Tarefa editada com sucesso!')
+        else:
+            break
+    else:
+        print('ID não encontrado! Por favor, tente novamente.')
+
+
 
 def visualizar_tarefas():
     tarefas = abrir_arquivo()
     print('--- Visualizar Tarefas ---')
     for tarefa in tarefas:
         print(f'ID: {tarefa["id"]}| Título: {tarefa["titulo"]}| Concluída: {tarefa["concluida"]}')
+        print('---------------'*3)
+    while True:
+        print('1 - Editar Tarefa')
+        print('0 - Sair')
+        opcao = valida_int('Opção: ')
+        if opcao == 1 or opcao == 0:
+            if opcao == 1:
+                concluir_tarefa(tarefas)
+            elif opcao == 0:
+                print('--Saindo do Sistema, Até mais!!--')
+                break
 
 
 

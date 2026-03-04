974a6c06c934354a35fc6337b1e3b4556d5d3679
Jamily Silva <dev.jamilyemanuele@gmail.com>
2026-03-04 13:58:14 -0300

    create basic crud for test sqlite3

--- Concepts/sqlite3/ex_db_1.py
+++ Concepts/sqlite3/ex_db_1.py
@@ -4,39 +4,49 @@ import sqlite3 as sql #importando sql para persistencia de dados
 # ex: a conexão é o tunel e o cursor é o carrinho trazendo e levando dados
 
 # UTILIZAR 
+def conexao_db():
+    conexao = sql.connect("Concepts/sqlite3/Escola.db")
+    # para não se preocupar em fechar a conexã, utilize o with como no JSON
+    # with sql.connect("Concepts/sqlite3/Escola.db") as coon:
+        # cursor = coon.cursor() # e ai voce vai seguindo em frente b
+    return conexao 
 
-conexao = sql.connect("Concepts/sqlite3/Escola.db")
-# para não se preocupar em fechar a conexã, utilize o with como no JSON
-# with sql.connect("Concepts/sqlite3/Escola.db") as coon:
-    # cursor = coon.cursor() # e ai voce vai seguindo em frente 
-cursor = conexao.cursor() # criar o cursor para executar comandos
+def create_table():
+    conn = conexao_db()
+    cursor = conn.cursor()
+    #pt 1 - CRIANDO O BD, INSERINDO DADOS E ATUALIZANDO
+    table = cursor.execute("""
+            CREATE TABLE IF NOT EXISTS alunos(
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    nome TEXT NOT NULL,
+                    idade INTEGER NOT NULL
+                    )
+            """)
+    conn.commit()
+    conn.close()
 
-#pt 1 - CRIANDO O BD, INSERINDO DADOS E ATUALIZANDO
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS alunos(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            nome TEXT NOT NULL,
-            idade INTEGER NOT NULL
-            )
-""")
+def cadastro_db(name, age):
+    conn = conexao_db()
+    cursor = conn.cursor()
 # INSERINDO dados
-cursor.execute("""
-    INSERT INTO alunos
-               (nome, idade)  VALUES ("jamily", 18), ("jay", 17)
-""")
-# SALVANDO alteração
-conexao.commit()
-#Fechando conexão
+    cursor.execute("""
+        INSERT INTO alunos
+                (nome, idade)  VALUES (?,?)""", (name, age))
+    # SALVANDO alteração
+    conn.commit()
+    #Fechando conexão
+    conn.close()
+    # conexao.close()
 
-# conexao.close()
+def read_db():
+    conn = conexao_db()
+    cursor = conn.cursor()
+    # PT 2 - LENDO DADOS 
+    cursor.execute("""  SELECT * FROM alunos""") # puxando todos os dados
+    resultados = cursor.fetchall() 
+    for linha in resultados:
+        print(f"ID: {linha [0]} | Nome: {linha [1]} | Idade: {linha [2]}")
 
-
-# PT 2 - LENDO DADOS 
-cursor.execute("""
-                SELECT * FROM alunos
-""") # puxando todos os dados
-resultados = cursor.fetchall() 
-
-for linha in resultados:
-    print(f"ID: {linha [0]} | Nome: {linha [1]} | Idade: {linha [2]}")
+    conn.commit()
+    conn.close()
 
--- Concepts/sqlite3/main.py
+++ Concepts/sqlite3/main.py
@@ -1,4 +1,43 @@
+from ex_db_1 import create_table, cadastro_db, read_db
 #executando o que foi aprendido 
 
 #nome
 #idade
+def valid_type(tipo, mensagem):
+    while True:
+        try:
+            valor = tipo(input(mensagem))
+            return valor
+        except ValueError:
+            print("Tipo de valor incorreto")
+    
+
+def cadastro():
+        print("---- Formulário ----")
+        name = input("Nome: ").lower()
+        age = valid_type(int, "Idade: ")
+        cadastro_db(name, age)
+
+
+def read_dates():
+    read_db()
+
+def menu():
+    create_table()
+    while True:
+        print("Bem-Vindo!")
+        print("1 - Cadastrar Usuário")
+        print("2 - Ver Dados")
+        print("0 - Sair")
+        opcao = valid_type(int, "opção: ")
+        if opcao == 1:
+            cadastro()
+        elif opcao == 2:
+            read_db()
+        elif opcao == 0:
+            print("Saindo")
+            break
+        else: 
+            print("Opção inexistente ou incorreta")
+
+menu()
